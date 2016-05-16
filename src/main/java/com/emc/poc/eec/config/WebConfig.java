5a695feca4cd5f75945d435054fe06c663949bd6
Simon O'Brien <simon@the-obriens.net>
2016-05-16 17:38:48 +0100

    Initial Project Commit

---
+++ src/main/java/com/emc/poc/eec/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.emc.poc.eec.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages={"com.emc.poc.eec"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+  
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    super.addResourceHandlers(registry);
+  }
+
+}
